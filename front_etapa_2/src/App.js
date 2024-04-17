83ba246572487399b437f8722e1d54a9c574c121
Juan David Salguero Medina <j.salguero@uniandes.edu.co>
2024-04-17 17:22:37 -0500

    completo

--- front_etapa_2/src/App.js
+++ front_etapa_2/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReviewInputManager from './components/c_review';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Review Submission App</h1>
+      <h1 id='titulo_app'>Review Master</h1>
       <ReviewInputManager />
     </div>
   );
--- front_etapa_2/src/components/c_review.js
+++ front_etapa_2/src/components/c_review.js
@@ -1,69 +1,104 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './c_review.css'
 
 function ReviewInputManager() {
-  const [inputs, setInputs] = useState([{ id: 1, text: '', result: null }]);
+
+  const initial_state = [{ id: 1, texto: '', resultado: null, probabilidad: null }];
+  const [entradas, setEntradas] = useState(initial_state);
 
   const handleAddClick = () => {
-    const newInputId = inputs.length + 1;
-    setInputs([...inputs, { id: newInputId, text: '', result: null }]);
+    const new_id = entradas.length + 1;
+    setEntradas(entradas.concat({ id: new_id, texto: '', resultado: null, probabilidad: null }));
   };
 
-  const handleInputChange = (id, text) => {
-    const newInputs = inputs.map(input => {
-      if (input.id === id) {
-        return { ...input, text: text };
+  const handleInputChange = (id, texto) => {
+    setEntradas(entradas.map(entrada => {
+      if (entrada.id === id) {
+        return { ...entrada, texto: texto };
       }
-      return input;
-    });
-    setInputs(newInputs);
+      return entrada;
+    }));
   };
 
   const handleRemoveClick = (id) => {
-    const newInputs = inputs.filter(input => input.id !== id);
-    setInputs(newInputs);
+    setEntradas(entradas.filter(entrada => entrada.id !== id));
   };
 
   const handleSubmit = () => {
-    const apiUrl = 'http://localhost:8000/predict';  // URL del backend
-    axios.post(apiUrl, { reviews: inputs.map(input => input.text) })
-      .then(response => {
-        const results = response.data.predictions;  // Asumiendo que el backend devuelve un objeto con una propiedad 'predictions'
-        updateResults(results);
+    const api_url = 'http://localhost:8000/predict_probs';
+    const datos_resenas = {
+      Review: entradas.map(entrada => entrada.texto),
+      Class: entradas.map(entrada => 0), // Asegúrate de ajustar este valor según la lógica de tu backend
+    };
+    axios.post(api_url, datos_resenas)
+      .then(respuesta => {
+        if (respuesta.data && respuesta.data.predictions && respuesta.data.probabilities) {
+          updateResults(respuesta.data.predictions, respuesta.data.probabilities);
+        } else {
+          console.log('Respuesta recibida, pero no hay predicciones ni probabilidades:', respuesta);
+        }
       })
       .catch(error => {
-        console.error('There was an error!', error);
+        console.error('¡Hubo un error!', error);
       });
   };
 
-  const updateResults = (results) => {
-    const newInputs = inputs.map((input, index) => ({
-      ...input,
-      result: results[index]  // Asegúrate de que cada resultado corresponda a su entrada
-    }));
-    setInputs(newInputs);
+  const updateResults = (predicciones, probabilidades) => {
+    setEntradas(entradas.map((entrada, indice) => ({
+      ...entrada,
+      resultado: predicciones[indice],
+      probabilidad: probabilidades[indice],
+    })));
+  };
+
+  const handleResetClick = () => {
+    setEntradas(initial_state);
   };
 
   return (
-    <div>
-      {inputs.map(input => (
-        <div key={input.id}>
+    <div id='container'>
+      {entradas.map(entrada => (
+        <div key={entrada.id} id='input_container'>
+
           <input
+            id='input_review'
             type="text"
-            value={input.text}
-            onChange={(e) => handleInputChange(input.id, e.target.value)}
-            placeholder="Enter review here..."
+            value={entrada.texto}
+            onChange={(e) => handleInputChange(entrada.id, e.target.value)}
+            placeholder="Ingrese su reseña aquí..."
           />
-          <button onClick={() => handleRemoveClick(input.id)}>Remove</button>
-          {input.result && <p>Prediction: {input.result}</p>}
-        </div>
+          <button id='submit_button' onClick={() => handleRemoveClick(entrada.id)}>Eliminar reseña</button>
+
+          <div id='resultados_container'>
+
+            <div id='resultados'>
+              {entrada.resultado && <p><strong>Predicción: </strong>{entrada.resultado}</p>}
+              {Array.from({ length: entrada.resultado }).map((_, i) => (
+              <p id='estrellas' key={i}>⭐</p>))}
+            </div>
+
+            <div id='probabilidades'>
+              {entrada.probabilidad && <p><strong>Probabilidad:</strong> {Number(entrada.probabilidad).toFixed(2)}</p>}
+            </div>
+
+          </div>
+          
+          </div>
       ))}
-      <button onClick={handleAddClick}>Add Another Review</button>
-      <button onClick={handleSubmit}>Submit Reviews</button>
+      <div id='button_container'>
+        <button id='add_button' onClick={handleAddClick}>+</button>
+        <p id='support_text'>Agregar otra reseña</p>
+        <button id='send_reviews' onClick={handleSubmit}>Enviar Reseñas</button>
+        {entradas.length > 1 && <button id='delete_reviews' onClick={handleResetClick}>Borrar Todo</button>}
+      </div>
+
     </div>
   );
 }
 
 export default ReviewInputManager;
+
+
 
 
